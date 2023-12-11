f475e5f12bc89299e4d2d81849b430e46d45269f
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-11 16:23:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.set("port", process.env.PORT || 3000)
 
+app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
