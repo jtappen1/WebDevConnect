86c4ef07c3e219487b011fe361e2badf328201fa
sethalley <sethalley32@gmail.com>
2023-12-11 14:52:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.set("port", process.envPORT || 3000)
+app.set("port", process.env.PORT || 3000)
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
