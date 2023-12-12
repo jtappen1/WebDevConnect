92c6e1ae9a1d612f4dde31dcbb9be34d721e737b
Sawyereva <106629658+Sawyereva@users.noreply.github.com>
2023-12-11 21:55:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const knex = require("knex")({
     client: "pg",
     connection: {
-        host: process.env.RDS_HOSTNAME || "ebroot",
-        user: process.env.RDS_USERNAME || "postgres",
+        host: process.env.RDS_HOSTNAME || "postgres",
+        user: process.env.RDS_USERNAME || "ebroot",
         password: process.env.RDS_PASSWORD || "ChickenJoe03",
         database: process.env.RDS_DB_NAME || "ebdb",
         port: process.env.RDS_PORT || 5432,
