aacbc74a3af55ea8023b67663b01f684055a7a6d
sethalley <sethalley32@gmail.com>
2023-12-11 17:09:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,19 +29,6 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-// Knex database connection
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: process.env.RDS_HOSTNAME || "localhost",
-        user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "password",
-        database: process.env.RDS_DB_NAME || "NormalizationTest",
-        port: process.env.RDS_PORT || 5432,
-        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
-    }
-});
-
 const pool = new Pool({
     user: 'your_user',
     host: 'your_host',
