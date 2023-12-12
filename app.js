a1d71093b37395715eeb77b863c5879432966a6e
sethalley <sethalley32@gmail.com>
2023-12-12 15:47:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("port", process.env.PORT || 3000)
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-const { Pool } = require('pg');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
