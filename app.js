0d07c7d43e1a0576f12e0f96511f6bd3eac5c58b
Sawyereva <106629658+Sawyereva@users.noreply.github.com>
2023-12-13 13:56:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/studlogin', async (req, res) => {
   
     try {
       // Query the database for a user with the provided email
-        const user = await knex('Students').select('*').where('Students.StudEmail', studEmail).first();
+        const user = await knex('Students').select('*').first();
         console.log(user);
       // Check if a user with the provided email was found
         if (user) {
