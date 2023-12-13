fc433b40104ff4a926e13ac35e367e00dcf3bd41
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-13 16:47:38 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d WebDevConnect.us-east-1.elasticbeanstalk.com  --nginx --agree-tos --email jassinfosystems@gmail.com
+
+sudo certbot -n -d WebDevConnect.is404.net  --nginx --agree-tos --email jassinfosystems@gmail.com
